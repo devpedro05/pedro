92c8719e23d6261867731a80e425d4f332c369b2
devpedro05 <pedrohenriqueribeiro0405@gmail.com>
2021-04-20 18:07:21 -0300

    Adição dos arquivos

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+let hamburguer = document.querySelector('.hamburguer')
+let nav = document.querySelector('nav')
+
+hamburguer.addEventListener('click', function(){
+    nav.classList.toggle('show-nav');
+ 
+})
